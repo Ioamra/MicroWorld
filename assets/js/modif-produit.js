0a42981b04b3de98ac0e7bd78f9e5966a6970e57
ioamra <iom.monchau@gmail.com>
2022-04-30 19:42:31 +0200

    pagination

--- assets/js/modif-produit.js
+++ assets/js/modif-produit.js
@@ -1,3 +1,16 @@
+$(function(){
+    let url = window.location;
+    let params = new URLSearchParams(url.search);
+    let page = params.get('page');
+    let numPage = document.getElementsByClassName('page-item');
+
+    for (i = 0; i < numPage.length; i++){
+        if ( numPage[i].firstChild.innerHTML == page) {
+            numPage[i].className = "page-item active";
+        }
+    }
+});
+
 function switchDispo(idProduit) {
     fetch('api.php?action=switchDispo&idProduit='+idProduit);
     window.location.reload();
